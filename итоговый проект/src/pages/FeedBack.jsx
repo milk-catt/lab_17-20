aebebb3dcba62c083df77b1f73b8cd3ad8069aa0
milk-catt <nastena.chekrizova@gmail.com>
2026-05-22 22:34:23 +0300

    ААА, Абезьяны, кАшелоты, ААА, крАкодилы, бегемоты

--- итоговый проект/src/pages/FeedBack.jsx
+++ итоговый проект/src/pages/FeedBack.jsx
@@ -14,22 +14,22 @@ function FeedBack() {
       <h2 className="page-title">📞 Обратная связь</h2>
       
       <div style={{ marginBottom: 20 }}>
-        <p>📱 Телефон: <strong>+7 908 517 50 43</strong></p>
-        <p>💬 Telegram: <strong>@softtoys</strong></p>
-        <p>📧 Email: <strong>info@softtoys.ru</strong></p>
+        <p>📱 Телефон: <strong>8-(800)-555-35-35 - Проще позвонить чем у кого-то занимать!😘</strong></p>
+        <p>💬 Telegram: <strong>FFFFFFFFFFF</strong></p>
+        <p>📧 Email: <strong>chekrizova@sfedu.ru, aleolei@sfedu.ru</strong></p>
       </div>
 
       <form onSubmit={handleSubmit} className="contact-form">
         <input 
           type="text" 
-          placeholder="Ваше имя" 
+          placeholder="ФИО" 
           value={form.name}
           onChange={(e) => setForm({...form, name: e.target.value})}
           required
         />
         <input 
           type="email" 
-          placeholder="Ваш Email" 
+          placeholder="Email" 
           value={form.email}
           onChange={(e) => setForm({...form, email: e.target.value})}
           required
